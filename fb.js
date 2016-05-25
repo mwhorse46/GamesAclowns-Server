ac912bf0c36a5c903aa76f528ba87bb7da4f8777
mwhorse46 <needmefordev@gmail.com>
2016-05-25 23:55:27 +0530

    Instance Change

--- fb.js
+++ fb.js
@@ -2,5 +2,5 @@
 module.exports = {
 	'appID' : '971412206281096',
 	'appSecret' : '34cf1ebea53dbf7c01ddcfd24e1f22c2',
-	'callbackUrl' : 'http://ec2-52-89-20-189.us-west-2.compute.amazonaws.com/login/facebook/callback'
+	'callbackUrl' : 'http://ec2-52-34-214-90.us-west-2.compute.amazonaws.com/login/facebook/callback'
 }
