f3c5188d9ea36844c1b298a066bf05575924f2f4
mwhorse46 <needmefordev@gmail.com>
2016-05-27 00:16:11 +0530

    Landing

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('index');
+        res.render('landing');
     });
 }
 
@@ -90,7 +90,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('index');
+    res.render('landing');
 });
 
 
