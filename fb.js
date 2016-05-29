490aa2581235f2369e2dd8a3146568be044eb107
mwhorse46 <needmefordev@gmail.com>
2016-05-30 01:26:02 +0530

    ADS TEST

--- fb.js
+++ fb.js
@@ -1,6 +1,6 @@
 // facebook app settings
 module.exports = {
-	'appID' : '1070226503051446',
-	'appSecret' : '0c31df175c6d0db96c2f00c603bcad0b',
+	'appID' : '1059351237466429',
+	'appSecret' : '83d4be524c09043c09031802c90fcf18',
 	'callbackUrl' : 'http://games.angryclowns.com/login/facebook/callback'
 }
