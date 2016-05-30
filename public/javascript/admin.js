2314da449c1a95db21f933f19ae9bf532ace2d33
mwhorse46 <needmefordev@gmail.com>
2016-05-30 17:23:58 +0530

    Gallery

--- public/javascript/admin.js
+++ public/javascript/admin.js
@@ -16,9 +16,16 @@ $('document').ready(function () {
 
     function setGallery(imageArray) {
 
+        var male    =   0, female   =   0;
         for(var i=0;i<imageArray.length;i++){
 
             var gender  =   imageArray[i].gender==1 ? "Male":"Female";
+
+            if(imageArray[i].gender == 1){
+                male++;
+            }else{
+                female++;
+            }
             var form    =   "<tr>"+
                 "<td>"+(i+1)+"</td>"+
                 "<td><img src='http://games.angryclowns.com/images/"+imageArray[i].image+"' width='100px' height='100px'></td>"+
@@ -28,5 +35,7 @@ $('document').ready(function () {
                 "</tr>";
             $("#table_container").append(form);
         }
+
+        $("#count_").text("MALE: "+male+" , FEMALE: "+female)
     }
 });
